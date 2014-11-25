2af59a9d836de2fd9619dad18334b662ab732334
Nick Brown <nick@nickbrown.me>
2014-11-24 21:16:52 -0800

    moved jquery and angular to npm' browserifying mainController

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,9 @@
 /*globals angular*/
 'use strict';
 
-require('../bower_components/angular/angular');
+//require('../bower_components/angular/angular');
+require('angular');
 
 var app = angular.module('sportStore', []);
-var mainController = require('./mainController');
-var fun = function($scope) {
-    $scope.name = 'Nick';
-    $scope.model = {
-        name: 'Nick'
-    };
-};
 
-var fun2 = require('./mainController')();
-
-app.controller('MainCtrl', ['$scope', fun2]);
+app.controller('MainCtrl', ['$scope', require('./mainController')]);
--- public/js/mainController.js
+++ public/js/mainController.js
@@ -1,8 +1,3 @@
-module.exports = function() {
-    return function($scope) {
-        $scope.name = 'Nick';
-        $scope.model = {
-            name: 'Nick'
-        };
-    };
+module.exports = function($scope) {
+    'use strict';
 };
