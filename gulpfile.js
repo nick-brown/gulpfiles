da4aab955238b6deb163042d41404706934d535c
Nick Brown <nick@nickbrown.me>
2014-11-24 21:43:46 -0800

    livereload added to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -8,53 +8,43 @@ var gulp = require('gulp')
 ,   concat = require('gulp-concat')
 ,   jshint = require('gulp-jshint')
 ,   stylish = require('jshint-stylish')
-,   csslint = require('gulp-csslint');
-//,   livereload = require('./app/livereload')
+,   csslint = require('gulp-csslint')
+,   livereload = require('gulp-livereload');
 //,   bodyParser = require('body-parser')
 //,   db = require('./config/db');
-//
 
 
 // TASKS
 //==================================================================
-gulp.task("default", function() {
-    gulp.watch("./public/**/*.scss", ["compile:css"]);
-    gulp.watch("./public/js/*.js", ["browserify"]);
-    //gulp.watch("./app/**/*.*", livereload);
+gulp.task('default', ['compile:js', 'compile:css', 'watch']);
+
+gulp.task('watch', function() {
+    var server = livereload();
+
+    gulp.watch('./public/**/*.scss', ['compile:css']);
+    gulp.watch('./public/js/*.js', ['compile:js']);
 });
 
-gulp.task("jshint", function() {
-    return gulp.src(["./public/js/**/*.js"])
+gulp.task('jshint', function() {
+    return gulp.src(['./public/js/**/*.js'])
       .pipe(jshint())
-      .pipe(jshint.reporter("jshint-stylish"))
+      .pipe(jshint.reporter('jshint-stylish'))
 });
 
-//gulp.task("compile:js", function() {
-//    return gulp.src('./public/js/*.js')
-//        .pipe(concat('all.js'))
-//        .pipe(gulp.dest('./dist/js'));
-//});
-
-gulp.task("browserify", ["jshint"], function() {
+gulp.task('compile:js', ['jshint'], function() {
     return browserify('./public/js/app')
         .bundle()
         .pipe(source('bundle.js'))
         .pipe(streamify(uglify()))
         .pipe(gulp.dest('./public/dist'))
+        .pipe(livereload());
 });
 
-//gulp.task("compile:js", ["jshint"], function() {
-//    // single point of entry for app
-//    var bundle = browserify("./src/js/main.js").bundle();
-//    return bundle
-//      .pipe(source("bundle.js"))
-//      .pipe(gulp.dest("./public/static/js/"));
-//});
-
-gulp.task("compile:css", function() {
-    return gulp.src(["./public/scss/*.scss"])
+gulp.task('compile:css', function() {
+    return gulp.src(['./public/scss/*.scss'])
         .pipe(sass())
         .pipe(csslint())
         .pipe(csslint.reporter())
-        .pipe(gulp.dest("./public/css"));
+        .pipe(gulp.dest('./public/css'))
+        .pipe(livereload());
 });
