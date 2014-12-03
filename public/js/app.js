055eb26853624efece4af3d66e55e754114f238a
Nick Brown <nick@nickbrown.me>
2014-12-02 22:24:31 -0800

    added unique filter and categories

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,13 @@
 /*globals angular*/
 'use strict';
 
-//require('../bower_components/angular/angular');
 require('angular');
 
 var app = angular.module('sportStore', []);
 
+// Controllers
 app.controller('MainCtrl', ['$scope', require('./controllers/mainController')]);
+
+// Filters
+//angular.module('customFilters', []).filter('uniq', require('./filters/customFilters.js'));
+app.filter('unique', require('./filters/customFilters.js'));
