808b46539a89d9263d13c6fd0e44263590464f7d
Nick Brown <nick@nickbrown.me>
2015-01-03 04:07:05 -0800

    added cart view; ng-route; and routes

--- public/js/app.js
+++ public/js/app.js
@@ -2,31 +2,51 @@
   'use strict';
 
   require('angular');
+  require('angular-route');
   require('./ngmodules/customFilters');
   require('./components/cart/cart');
 
   // Modules
   //===========================================================================
-  var app = angular.module('sportStore', ['customFilters', 'cart']);
+  var app = angular.module('sportStore', ['customFilters', 'cart', 'ngRoute']);
+
+  // Modules
+  //===========================================================================
+  app
+    // takes a function which is executed when the module is loaded but before
+    // the application is executed -- good for one-off configuration tasks
+    .config(function($routeProvider) {
+        $routeProvider.when('/checkout', {
+            templateUrl: '/js/views/checkoutSummary.html'
+        });
+
+        $routeProvider.when('/products', {
+            templateUrl: '/js/views/productList.html'
+        });
+
+        $routeProvider.otherwise({
+            templateUrl: '/js/views/productList.html'
+        });
+    });
 
   // Controllers
   //===========================================================================
   app
-      .controller('MainCtrl', ['$scope', '$http', 'dataUrl', require('./controllers/mainController')])
-      .controller('ProductListCtrl', [
-          '$scope',
-          '$filter',
-          'productListActiveClass',
-          'productListPageCount',
-          'cart',
-          require('./controllers/productListControllers')
-      ]);
+    .controller('MainCtrl', ['$scope', '$http', 'dataUrl', require('./controllers/mainController')])
+    .controller('ProductListCtrl', [
+        '$scope',
+        '$filter',
+        'productListActiveClass',
+        'productListPageCount',
+        'cart',
+        require('./controllers/productListControllers')
+    ]);
 
 
   // Constants
   //===========================================================================
   app
-      .constant('productListPageCount', 3)
-      .constant('dataUrl', 'http://localhost:8080/api/products')
-      .constant('productListActiveClass', 'btn-primary');
+    .constant('productListPageCount', 3)
+    .constant('dataUrl', 'http://localhost:8080/api/products')
+    .constant('productListActiveClass', 'btn-primary');
 }());
