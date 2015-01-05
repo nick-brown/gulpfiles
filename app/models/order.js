3915954d94f1caa569616b5af6827dbfd7692618
Nick Brown <nick@nickbrown.me>
2015-01-05 00:22:19 -0800

    added required status to order schema

--- app/models/order.js
+++ app/models/order.js
@@ -2,15 +2,15 @@ var mongoose   = require('mongoose')
 ,   Schema     = mongoose.Schema;
 
 var OrderSchema = new Schema({
-    name     : String,
-    street   : String,
-    state    : String,
-    zip      : String,
-    country  : String,
-    giftwrap : { type: Boolean, default: false },
-    products : Array,
-    created  : { type: Date, default: Date.now() },
-    updated  : { type: Date, default: Date.now() }
+    name     : { type: String  , required: true },
+    street   : { type: String  , required: true },
+    state    : { type: String  , required: true },
+    zip      : { type: String  , required: true },
+    country  : { type: String  , required: true },
+    products : { type: Array   , required: true },
+    giftwrap : { type: Boolean , default : false },
+    created  : { type: Date    , default : Date.now() },
+    updated  : { type: Date    , default : Date.now() }
 });
 
 module.exports = mongoose.model('Order', OrderSchema);
